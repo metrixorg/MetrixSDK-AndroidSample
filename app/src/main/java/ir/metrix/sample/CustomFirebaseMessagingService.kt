1bab6f23f2e44886b7a9448cd543b9fb2efecab2
Salman Qadami <salmanqadami@gmail.com>
2024-08-11 20:21:25 +0330

    update SDK version to v2.2.8

---
+++ app/src/main/java/ir/metrix/sample/CustomFirebaseMessagingService.kt
@@ -0,0 +1,22 @@
+package ir.metrix.sample
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import ir.metrix.notification.MetrixNotification
+
+class CustomFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        Log.d("messaging", "onNewToken: $token")
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d("messaging", "onMessageReceived: ${message.messageId}, ${message.sentTime}")
+        Log.d("messaging", message.data.toString())
+
+        if (MetrixNotification.onMessageReceived(message)) return
+        // here you can process your own fcm messages
+    }
+}
